f0fb45b7bd035af6f2514287729f16aa82dccd35
Fajri <fajriramadhan71717@gmail.com>
2025-12-19 19:02:34 +0700

    Add initial C++ program for Praktikum Pengenalan Pemrogramman

---
+++ Pertemuan2PP.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nama = "--Fajri Ramadhan--";
+    string NPM = "--2517051041--";
+
+    cout << "Selamat Datang Di Praktikum Pengenalan Pemrogramnan" << endl;
+    cout << "Nama saya adalah " << nama << endl;
+    cout << "NPM saya adalah " << NPM << endl << endl;
+
+    cout << "Unila Indah" << endl;
+    cout << "Be Strong" << endl;
+}
